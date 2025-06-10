9db9c7617935b4309a1487e177b03a3635f75e2c
Fachruddin <poenyoex@gmail.com>
2025-06-10 23:55:08 +0700

    Format code in dokter.php for improved readability and consistency

--- routes/dokter.php
+++ routes/dokter.php
@@ -8,13 +8,17 @@ Route::middleware(['auth', 'role:dokter'])->prefix('dokter')->group(function () 
     Route::get('/dashboard', function () {
         return view('dokter.dashboard');
     })->name('dokter.dashboard');
-    
+
+    // Resource routes Obat for Dokter
     Route::resource('obat', ObatController::class, [
         'as' => 'dokter'
     ])->except(['show']);
 
+    // Resource routes Jadwal Periksa for Dokter
     Route::resource('jadwal', JadwalPeriksaController::class, [
         'as' => 'dokter'
     ])->except(['show']);
+    
+    // Custom route to toggle status of Jadwal Periksa
     Route::patch('jadwal/{jadwal}/toggle-status', [JadwalPeriksaController::class, 'toggleStatus'])->name('dokter.jadwal.toggle-status');
 });
